06c777f3b3371ac6e8d8dd38fac9cf57ca99733b
Azeez Adebayo <azeez.adedayo@yahoo.com>
2019-12-16 23:53:49 +0100

    Added Created On, Modified On, Created By and Modified By properties to Product object

--- Inventory.Model/ProductDetail.cs
+++ Inventory.Model/ProductDetail.cs
@@ -22,16 +22,32 @@ namespace Inventory.Model
         [Display(Name = "Product Name")]
         public string ProductName { get; set; }
 
+
         [Required]
-        public double Price { get; set; }
+        [Display(Name = "Cost Price")]
+        public double CostPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Selling Price")]
+        public double SellingPrice { get; set; }
 
         public string Flag { get; set; }
 
+        [Required]
+        [Display(Name = "Created By")]
         public string CreatedBy { get; set; }
+
+        [Display(Name = "Modified By")]
+        public string ModifiedBy { get; set; }
 
 
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-        public DateTime KeyDate { get; set; }
+        [Display(Name = "Created On")]
+        public DateTime CreatedOn { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Modified On")]
+        public DateTime ModifiedOn { get; set; }
 
         public virtual ProductCategory ProductCategory { get; set; }
 
